98a31b140d33b69849b5752ddc0f2ad677b6b153
MatveySakharuk <matvey.sakharuk@wayflyer.com>
2025-10-08 15:34:34 +0200

    EF-641: dynamic theme for the reference app

---
+++ src/config/partnersThemes.ts
@@ -0,0 +1,6 @@
+import type { PartnerTheme } from '../types';
+
+export const PARTNER_THEMES: PartnerTheme[] = [
+  { theme: "wayflyer", partnerId: "HbLL6T282akhR4gopGWLu1wU61Dk4Wg6" },
+  { theme: "whiteLabel", partnerId: "VhNza7TvrW5CeqvfIuQMpFEmfRTclVYJ" },
+];
