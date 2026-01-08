3ba5b7444a4fd35c8df9f5d12cb289395c5b0d0a
MatveySakharuk <matvey.sakharuk@wayflyer.com>
2026-01-08 13:54:44 +0100

    add client side navigation to the reference app (#49)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { BrowserRouter } from "react-router";
+
 import { QueryProvider } from './components/query-provider/QueryProvider.tsx';
 import { ContentManager } from "./components/content-manager/ContentManager.tsx";
 
 export const App = () => {
 
   return (
-    <QueryProvider>
-      <ContentManager />
-    </QueryProvider>
+    <BrowserRouter>
+      <QueryProvider>
+        <ContentManager />
+      </QueryProvider>
+    </BrowserRouter>
   );
 };
--- src/components/main-content/MainContent.tsx
+++ src/components/main-content/MainContent.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { AppShell } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { Routes, Route } from "react-router";
 
 import { Header } from "../header/Header.tsx";
 import { Navigation } from "../navigation/Navigation.tsx";
-import { Dashboard } from "../../pages/dashboard.tsx";
+import { Dashboard, Products } from "../../pages";
 import { THEME_CONFIG, type ThemeTokens, type Theme } from "../select-theme/theme.ts";
 
 type MainContentProps = {
@@ -28,17 +29,16 @@ export const MainContent = ({ partnerDesignId }: MainContentProps) => {
       padding="md"
     >
       <AppShell.Header>
-        <Header
-          theme={theme}
-          opened={opened}
-          toggle={toggle}
-        />
+        <Header theme={theme} opened={opened} toggle={toggle} />
       </AppShell.Header>
       <AppShell.Navbar p="md">
         <Navigation />
       </AppShell.Navbar>
       <AppShell.Main bg={tokens.appBg}>
-        <Dashboard />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
       </AppShell.Main>
     </AppShell>
   );
